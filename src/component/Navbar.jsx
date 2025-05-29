635aedbc8287a8852f78e38d3fd4a9d147fed61e
FATIMAH ASIRI <fatimahasiri@FATIMAHs-MacBook-Pro-2.local>
2025-05-29 07:47:50 +0300

    fixed

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -35,7 +35,7 @@ export default function Navbar() {
 
   return (
     <nav className="bg-white shadow-md w-full z-10">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex justify-around items-center">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex justify-between items-center">
         <div className="inline-flex items-center">
 				<div className="text-xl font-bold text-[#00473e]">An </div>
 			    <img src="/logoh.png" className="w-5 h-5" />
